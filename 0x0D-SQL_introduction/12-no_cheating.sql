a70b642db1a2ccbad64ca808ebd1f4f26fc3d0b5
root <root@DESKTOP-0EKPDUF.localdomain>
2022-08-19 16:15:26 +0100

    mysql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
-cript that updates the attribute value of a table
+-- Script that updates the attribute value of a table
 -- Query to update the score of Bob to 10 in the table second_table
 UPDATE second_table
 SET score = 10
